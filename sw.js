2a74e07830c4b92ef168a0f1f401c3bab87216f7
hsskim12 <knowledge.hs@gmail.com>
2026-09-18 23:15:11 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* 홈 화면 설치와 오프라인 대비용 파일.
    화면 파일은 항상 인터넷에서 먼저 받아오고(새로 올린 내용이 바로 보이도록),
    인터넷이 안 될 때만 저장해 둔 것을 보여 줍니다. 일정 데이터(구글)는 건드리지 않습니다. */
-const CACHE = 'choir-notice-v29';
+const CACHE = 'choir-notice-v30';
 const SHELL = ['./', 'index.html', 'manifest.json', 'icon-32.png', 'icon-180.png', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
